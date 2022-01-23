f6ec06d89a5d7d6af0ef1c5cbf3bf02c00e5589d
maneeshc1497 <maneeshmjh13108@gmail.com>
2022-01-23 19:49:49 +0530

    Successfuly setup the react router to Veggie-Rescue Project

---
+++ frontend/src/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+function Login() {
+ 
+  return <div>
+      <h1>LOGIN PAGE</h1> 
+      <p>User Needs to login to see the Home Page</p>
+      {/* Here can add code to ask user to login and function to redirect to Home page if successfuly loggedin */}
+    </div>;
+}
+
+export default Login;
