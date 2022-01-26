7ebf0326a8e27b754f67992e2efff7cc6ffbfbc0
Ella Buysse <ellabuysse@Ellas-MacBook-Air.local>
2022-01-26 10:44:53 -0800

    deleted .idea folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
-    <Dashboard />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
