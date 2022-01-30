1c52773c28de349d0cc4691c4fa403e984c002cd
Rachel Izenson <rjizenson@gmail.com>
2022-01-30 11:34:36 -0800

    Add jest testing

---
+++ backend/nothing.test.ts
@@ -0,0 +1,10 @@
+function sum(a, b) {
+  return a + b;
+}
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+test("jest tests to work", () => {
+  expect(true).toBeTruthy();
+});
