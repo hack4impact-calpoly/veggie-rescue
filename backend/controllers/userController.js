a16c9df504434da8ef7dc4d8a15c0da98ee676d1
naspiras <naspiras@calpoly.edu>
2022-02-06 22:03:07 -0800

    Fixed backend and added necessary changes to server.ts and added schemas

---
+++ backend/controllers/userController.js
@@ -0,0 +1,27 @@
+const asyncHandler = require('express-async-handler')
+
+// @desc Register a new user
+// @route /api/users
+// @access Public
+
+const registerUser = asyncHandler(async(req, res) => {
+    const { name, email, password } = req.body
+
+    if(!name || !email || !password){
+        res.status(400)
+        throw new Error('Please include all fields')
+    }
+    res.send('Register Route')
+})
+// @desc Login a new user
+// @route /api/users
+// @access Public
+
+const loginUser = asyncHandler(async(req, res) => {
+    res.send('Login Route')
+})
+
+module.exports = {
+    registerUser,
+    loginUser,
+}
