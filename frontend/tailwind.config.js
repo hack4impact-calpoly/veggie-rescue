183101f3ebc4dd07b2e74f84dd6b6ebc1dc30cef
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2022-02-13 18:11:43 -0800

    resolving conflict in tailwind.confog.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
@@ -9,5 +7,5 @@ module.exports = {
       }
     },
   },
-  plugins: [],
-}
+  plugins: []
+};
