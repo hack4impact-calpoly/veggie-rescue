0ead55e3c470f8aad3d0189e0d23cd56d297e420
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2022-03-02 16:44:21 -0800

    converted boostrap to tailwind for adminloginscreen and navbar

--- frontend/src/components/AdminLoginScreen/AdminLoginScreen.tsx
+++ frontend/src/components/AdminLoginScreen/AdminLoginScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './AdminLoginScreen.css';
+// import './AdminLoginScreen.css';
 import { useEffect, useState, ChangeEvent, FormEvent } from 'react';
 import logo from '../../imgs/veggie-rescue-logo.png';
 
@@ -34,19 +34,22 @@ const AdminLoginScreen = () => {
   };
 
   return(
-    <div className="container">
-      <div className="text-box">
-        <div className="logo">
-          <img className='logo' src={logo} alt="veggie rescue logo" />
+    
+    <div className="bg-[url('imgs/carrot-background.png')] font-Poppins w-full h-screen relative flex items-center justify-center flex-col">
+      <div className="bg-white w-[460px] h-[525px] rounded-[15px] shadow-[0_2px_20px_2px_rgba(0,0,0,0.2)] flex flex-col items-center justify-center">
+        <div className="w-[350px] pt-[0px] pb-[15px]">
+          <img src={logo} alt="veggie rescue logo" />
+        </div>
+        <div className="border-t-[4px] border-solid border-[#FF9C55] w-[350px] flex items-center justify-center">
+          <h4 className='text-xs text-gray-700 mt-2'>
+            Sign in and start managing Veggie Rescue logs
+          </h4>
         </div>
 
-        <div className="instructions">
-          <h4>Sign in and start managing Veggie Rescue logs</h4>
-        </div>
-
-        <div className="inputs">
-          <div className="input-box">
+        <div className="rounded-10 flex items-center justify-center flex-col mt-5">
+        <div className="rounded-[30px] h-[55px]">
             <input
+              className="h-[35px] rounded-[30px] w-[18.75rem] border-2 border-solid border-[#929292] p-[3px] pl-[30px]"
               type="email"
               name="email"
               placeholder="Email"
@@ -54,8 +57,9 @@ const AdminLoginScreen = () => {
               onChange={handleEmailChange}
             />
           </div>
-          <div className="input-box">
+          <div className="rounded-[30px] h-[55px]">
             <input
+              className="h-[35px] rounded-[30px] w-[18.75rem] border-2 border-solid border-[#929292] p-[3px] pl-[30px]"
               type="password"
               name="password"
               placeholder="Password"
@@ -63,8 +67,8 @@ const AdminLoginScreen = () => {
               onChange={handlePasswordChange}
             />
           </div>
-          <div className="input-box">
-            <button type="submit" className="submit" onClick={handleSubmit}>
+          <div className="rounded-[30px] h-[55px]">
+            <button type="submit" className="rounded-[30px] w-[335px] border-none bg-orange-400 text-white h-12 flex items-center justify-center" onClick={handleSubmit}>
               <h3>Login</h3>
             </button>
           </div>
