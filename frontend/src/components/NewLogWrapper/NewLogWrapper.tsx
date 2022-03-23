50af23ee68c2a64c46b94c5c1ca95da5212dea3d
ssehnert <samuel.sehnert@gmail.com>
2022-03-23 13:57:22 -0700

    logic for pickupDelivery works. Looks nice as well. Moving on

--- frontend/src/components/NewLogWrapper/NewLogWrapper.tsx
+++ frontend/src/components/NewLogWrapper/NewLogWrapper.tsx
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
 
 import NavBar from '../NavBar/NavBar'
+import PickupDelivery from '../PickupDelivery/PickupDelivery'
 import Location from '../Locations/Location'
 import Weight from '../Weight/Weight'
 
 export default function NewLogWrapper() {
 
     const [wrapperCurrentPosition, setWrapperCurrentPosition] = useState<number>(0);
+    const [isPickup, setIsPickup] = useState<boolean>(false);
 
     return (
         <div className='container'>
             <NavBar setWrapperCurrentPosition={setWrapperCurrentPosition}/>
             {wrapperCurrentPosition === 0 && 
-            <div>Nice</div>
+                <PickupDelivery setIsPickup={setIsPickup} />
             }
             {wrapperCurrentPosition === 1 && 
                 <Location />
