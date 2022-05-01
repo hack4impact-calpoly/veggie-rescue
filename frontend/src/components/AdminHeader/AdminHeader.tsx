c02412d402d1485878811a41056159b40d974f22
Howl1935 <travisphawley@gmail.com>
2022-05-01 10:17:18 -0700

    Added logout to admin page

--- frontend/src/components/AdminHeader/AdminHeader.tsx
+++ frontend/src/components/AdminHeader/AdminHeader.tsx
@@ -1,11 +1,26 @@
+import React, { useState, useEffect } from 'react';
+
 import veggie_logo from '../../imgs/veggie-rescue-logo.png';
 import admin_profile_image from '../../imgs/admin_profile.png'
 import { useNavigate } from 'react-router-dom';
+import { useAppSelector, useAppDispatch } from '../../app/hooks';
+
+import {
+    clearAuth
+  } from '../../features/adminAuth/adminAuthSlice'
+import { toast } from 'react-toastify';
 
 export default function AdminHeader() {
     const navigate = useNavigate();
+    const dispatch = useAppDispatch();
+
+    const { admin } = useAppSelector((state)=> state.adminAuth)
     function handleDataBtn(){
      navigate('/Admin')
+    }
+    const handleLogout = async () =>{
+        await dispatch(clearAuth());
+        navigate('/AdminLogin')
     }
     return (
         <div>
@@ -38,8 +53,9 @@ export default function AdminHeader() {
                         </button>
                     </div>
                 </div>
-                <div className='px-10 flex items-center'> 
-                    <button><img src={admin_profile_image} alt="profile_image" onClick={() => console.log("Profile Picture Clicked")} className='w-10 h-10' /></button>
+                <div className='flex px-10  items-center'> 
+               
+                    <button onClick={handleLogout}  className='flex flex-row items-center '><div className='flex flex-col pr-6 text-white items-end'><span>Welcome <span className='text-orange-500'>{admin.name}!</span></span><span>Sign Out</span></div><img src={admin_profile_image} alt="profile_image" className='w-10 h-10' /></button>
                 </div>
 	        </nav>
         </div>    
