adc401e2e5b166de400784be681bbacf08874d7b
sophsuan <sophiahsuan888@gmail.com>
2022-05-07 16:36:24 -0700

    admin server_url

--- frontend/src/features/driverAuth/driverAuthService.tsx
+++ frontend/src/features/driverAuth/driverAuthService.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API_URL = '/api/drivers/';
-const SERVER_URL = process.env.REACT_APP_SERVER_URL ||  ''
+const SERVER_URL = process.env.REACT_APP_SERVER_URL ||  '';
 
 interface DriverData {
   _id: string,
