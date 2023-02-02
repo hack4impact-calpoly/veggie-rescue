ba5574e08fcf928e6018793fee91f60b2735532f
lemezze <letimezzetti@gmail.com>
2023-02-02 15:37:04 -0800

    Created skeleton for driverSummary

---
+++ frontend/src/components/driverSummary/driverSummary.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function DriverSummary() {
+  return <div>
+      <h1>Driver Summary</h1>
+      <p> You are now punching out. </p>
+
+      <p> Weight: </p>
+
+      <p> Write any discrepancies here: </p>
+
+    <button> Sign Out </button>
+
+    </div>;
+}
+
+export default DriverSummary;
