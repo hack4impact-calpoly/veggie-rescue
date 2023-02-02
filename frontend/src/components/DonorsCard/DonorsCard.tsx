8c5cb5d7aa7e3bcfd686381990419a1c9561a4ec
Kasey Liu <mermaidmagic99@gmail.com>
2023-02-02 13:19:22 -0800

    fixed formatting

--- frontend/src/components/DonorsCard/DonorsCard.tsx
+++ frontend/src/components/DonorsCard/DonorsCard.tsx
@@ -2,32 +2,34 @@ import React, { useLayoutEffect, useState } from 'react';
 import './DonorsCard.css';
 
 const DonorsCard = (props: any) => {
-  const sendData = () =>{
-    console.log(props.donor)
-    props.handleShow()
-    props.donorHandler(props.donor)
-  }
-  return(
-    <button onClick={sendData} >
-      <div className = "donors-card">
-        <div className="donor-name">{ props.donor.name }</div>
+  const sendData = () => {
+    console.log(props.donor);
+    props.handleShow();
+    props.donorHandler(props.donor);
+  };
+  return (
+    <button onClick={sendData}>
+      <div className="donors-card">
+        <div className="donor-name">{props.donor.name}</div>
         <div className="entry-container">
-        <div className="donors-entry">
-          <div className="donors-entry-title">Organizational Structure</div>
-          <div className="donors-entry-text">{ props.donor.EntityType }</div>
+          <div className="donors-entry">
+            <div className="donors-entry-title">Organizational Structure</div>
+            <div className="donors-entry-text">{props.donor.EntityType}</div>
+          </div>
+          <div className="donors-entry">
+            <div className="donors-entry-title">Location Type</div>
+            <div className="donors-entry-text">{props.donor.LocationType}</div>
+          </div>
+          <div className="donors-entry">
+            <div className="donors-entry-title">Combined Area Name</div>
+            <div className="donors-entry-text">
+              {props.donor.CombinedAreaName}
+            </div>
+          </div>
         </div>
-        <div className="donors-entry">
-          <div className="donors-entry-title">Location Type</div>
-          <div className="donors-entry-text">{ props.donor.LocationType }</div>
-        </div>
-        <div className="donors-entry">
-          <div className="donors-entry-title">Combined Area Name</div>
-          <div className="donors-entry-text">{ props.donor.CombinedAreaName }</div>
-        </div>
-        </div>
-    </div>
+      </div>
     </button>
   );
-}
+};
 
 export default DonorsCard;
--- frontend/src/components/RecipientsCard/RecipientsCard.tsx
+++ frontend/src/components/RecipientsCard/RecipientsCard.tsx
@@ -2,27 +2,33 @@ import React, { useLayoutEffect, useState } from 'react';
 import './RecipientsCard.css';
 
 const RecipientsCard = (props: any) => {
-  const sendData = () =>{
-    console.log(props.recipient)
-    props.handleShow()
-    props.recipientHandler(props.recipient)
-  }
-  return(
+  const sendData = () => {
+    console.log(props.recipient);
+    props.handleShow();
+    props.recipientHandler(props.recipient);
+  };
+  return (
     <button onClick={sendData}>
-      <div className = "donors-card">
-        <div className="donor-name">{ props.recipient.name }</div>
+      <div className="donors-card">
+        <div className="donor-name">{props.recipient.name}</div>
         <div className="entry-container">
           <div className="donors-entry">
             <div className="donors-entry-title">Organizational Structure</div>
-            <div className="donors-entry-text">{ props.recipient.EntityType }</div>
+            <div className="donors-entry-text">
+              {props.recipient.EntityType}
+            </div>
           </div>
           <div className="donors-entry">
             <div className="donors-entry-title">Demographics Served</div>
-            <div className="donors-entry-text">{ props.recipient.DemographicName }</div>
+            <div className="donors-entry-text">
+              {props.recipient.DemographicName}
+            </div>
           </div>
           <div className="donors-entry">
             <div className="donors-entry-title">Combined Area Name</div>
-            <div className="donors-entry-text">{ props.recipient.CombinedAreaName }</div>
+            <div className="donors-entry-text">
+              {props.recipient.CombinedAreaName}
+            </div>
           </div>
           <div className="donors-entry">
             <div className="donors-entry-title">Food Distribution Model</div>
@@ -32,6 +38,6 @@ const RecipientsCard = (props: any) => {
       </div>
     </button>
   );
-}
+};
 
 export default RecipientsCard;
