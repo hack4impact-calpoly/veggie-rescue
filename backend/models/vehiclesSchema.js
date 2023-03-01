c38a17b48ead46191c21968549cfd08b099e69f2
esauerbo <esauerbo@calpoly.edu>
2023-02-28 19:24:20 -0800

    chore: updating schema

--- backend/models/vehiclesSchema.js
+++ backend/models/vehiclesSchema.js
@@ -8,7 +8,7 @@ const VehicleSchema = new mongoose.Schema(
     img: String,
     currentPickups: Array,
     currentDropoffs: Array,
-    foodAllocation: {
+    totalFoodAllocation: {
       type: Map,
       of: Number,
     },
