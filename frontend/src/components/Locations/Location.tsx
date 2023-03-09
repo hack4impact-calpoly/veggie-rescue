28444a3033511d6bc0218ffede6809a59fac5c2e
oprylord <jtang72@calpoly.edu>
2023-03-09 00:07:49 -0800

    chore: removed adding newLocation

--- frontend/src/components/Locations/Location.tsx
+++ frontend/src/components/Locations/Location.tsx
@@ -1,98 +1,109 @@
 import { useState, useEffect } from 'react';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { toast } from 'react-toastify';
 import { useAppSelector, useAppDispatch } from '../../app/hooks';
 
-import { pickupSchema } from "./dbMock"; //import data from dbMock
-import LocationFilter from "./LocationFilter";
-import LocationForm from "./LocationForm";
-import Locations from "./Locations";
-import { toast } from 'react-toastify';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { pickupSchema } from './dbMock'; // import data from dbMock
+import LocationFilter from './LocationFilter';
+// import LocationForm from './LocationForm'; // removed as of issue 152
+import Locations from './Locations';
 
 import Spinner from '../Spinner/Spinner';
 
-import './Location.css'
-import {
-  getDonors
-} from '../../features/donors/donorSlice';
-import {
-  getRecipients
-} from '../../features/recipients/recipientsSlice';
+import './Location.css';
+import { getDonors } from '../../features/donors/donorSlice';
+import { getRecipients } from '../../features/recipients/recipientsSlice';
 
-interface locale {
-	name: string,
-	LocationType: string,
-	EntityType: string,
-	FoodType: string,
-    DemographicName: string,
+interface Locale {
+  name: string;
+  LocationType: string;
+  EntityType: string;
+  FoodType: string;
+  DemographicName: string;
 
-	CombinedAreaName: string,
-	_id: string,
+  CombinedAreaName: string;
+  _id: string;
 }
-interface Donor {
-	name: string,
-	LocationType: string,
-	EntityType: string,
-	FoodType: string,
-	CombinedAreaName: string,
-	_id: string,
-  }
 
-interface pickupDeliveryObjectSchema {
-    pickupOrDelivery: number,
-    id: String,
-    date: String,
-    driver: String,
-    vehicle: String,
-    name: String,
-    EntityType: String,
-    LocationType: String,
-    Demographic: String,
-    FoodType: String,
-    Area: String,
-    lbsDroppedOff: number
+interface PickupDeliveryObjectSchema {
+  pickupOrDelivery: number;
+  id: String;
+  date: String;
+  driver: String;
+  vehicle: String;
+  name: String;
+  EntityType: String;
+  LocationType: String;
+  Demographic: String;
+  FoodType: String;
+  Area: String;
+  lbsDroppedOff: number;
 }
 
 interface Props {
-  setPickupDeliveryObject : Function
-  PickupDeliveryObject    : pickupDeliveryObjectSchema,
-  setForceNext            : Function
+  setPickupDeliveryObject: Function;
+  PickupDeliveryObject: PickupDeliveryObjectSchema;
+  setForceNext: Function;
 }
 
-function Location({setPickupDeliveryObject, PickupDeliveryObject, setForceNext} : Props) {
+function Location({
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  setPickupDeliveryObject,
+  PickupDeliveryObject,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  setForceNext
+}: Props) {
   // Elements we will keep in local state and pass back and forth to components
   const [locations, setLocations] = useState([]);
-  const [current, setCurrent] = useState< undefined | locale>();
-  const [filtered, setFiltered] = useState<undefined | locale[]>();
+  const [current, setCurrent] = useState<undefined | Locale>();
+  const [filtered, setFiltered] = useState<undefined | Locale[]>();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [createNew, setCreateNew] = useState(false);
   const dispatch = useAppDispatch();
 
-    // Get the donors object from the store
+  // Get the donors object from the store
   const {
     donors,
-    isLoading : donorLoading,
-    isSuccess : donorSuccess
+    isLoading: donorLoading,
+    isSuccess: donorSuccess
   } = useAppSelector((state) => state.donors);
-    const {
-    recipients, 
-    isLoading : recipientLoading,
-    isSuccess : recipientSuccess
+  const {
+    recipients,
+    isLoading: recipientLoading,
+    isSuccess: recipientSuccess
   } = useAppSelector((state) => state.recipients);
 
-useEffect(()=>{
-  if(PickupDeliveryObject.pickupOrDelivery === 1 && donors.length === 0)
-  {
-    dispatch(getDonors());
-  }else if(PickupDeliveryObject.pickupOrDelivery === 2 && recipients.length === 0){
-    dispatch(getRecipients())
-  }
-  if(PickupDeliveryObject.pickupOrDelivery === 1 && donorSuccess ){
-    setLocations(donors)
-  }else if(PickupDeliveryObject.pickupOrDelivery === 2 && recipientSuccess){
-    setLocations(recipients)
-  }
-
-},[PickupDeliveryObject.pickupOrDelivery, dispatch, donorSuccess, donors, donors.length, recipientSuccess, recipients, recipients.length])
+  useEffect(() => {
+    if (PickupDeliveryObject.pickupOrDelivery === 1 && donors.length === 0) {
+      dispatch(getDonors());
+    } else if (
+      PickupDeliveryObject.pickupOrDelivery === 2 &&
+      recipients.length === 0
+    ) {
+      dispatch(getRecipients());
+    }
+    if (PickupDeliveryObject.pickupOrDelivery === 1 && donorSuccess) {
+      setLocations(donors);
+    } else if (
+      PickupDeliveryObject.pickupOrDelivery === 2 &&
+      recipientSuccess
+    ) {
+      setLocations(recipients);
+    }
+  }, [
+    PickupDeliveryObject.pickupOrDelivery,
+    dispatch,
+    donorSuccess,
+    donors,
+    donors.length,
+    recipientSuccess,
+    recipients,
+    recipients.length
+  ]);
   // This will create a temp copy of current location which will eventually be the item stored in vehicle database
-  const [savedLocation, setLocation] = useState<undefined | locale>();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [savedLocation, setLocation] = useState<undefined | Locale>();
 
   // Empties the filtered array called in:
   const clearFilter = () => {
@@ -105,29 +116,28 @@ useEffect(()=>{
   };
 
   // Uses array of locations and filters based on input into LocationFilter text area.
-  const filterLocations = (element : string) => {
-    const filtered : locale[] = locations.filter((loc : locale) => {
-                const regex = new RegExp(`${element}`, "gi");
-                return loc.name.match(regex);
-              });
+  const filterLocations = (element: string) => {
+    // eslint-disable-next-line @typescript-eslint/no-shadow
+    const filtered: Locale[] = locations.filter((loc: Locale) => {
+      const regex = new RegExp(`${element}`, 'gi');
+      return loc.name.match(regex);
+    });
     setFiltered(filtered);
   };
-if(donorLoading || recipientLoading)
-{
-  return <Spinner />
-}
+  if (donorLoading || recipientLoading) {
+    return <Spinner />;
+  }
   // We display the location filter, followed by the locations returned from filter results.  If there is a current location in state, then we display LocationForm component
   // otherwise it will not be displayed
   return (
     <div className="">
-
       {!current && (
         <LocationFilter
-        filtered={filtered}
-        clearFilter={clearFilter}
-        filterLocations={filterLocations}
-        clearCurrent={clearCurrent}
-      />
+          filtered={filtered}
+          clearFilter={clearFilter}
+          filterLocations={filterLocations}
+          clearCurrent={clearCurrent}
+        />
       )}
 
       <Locations
@@ -138,16 +148,17 @@ if(donorLoading || recipientLoading)
         clearFilter={clearFilter}
         setCreateNew={setCreateNew}
       />
+      {/* removed as of issue 152
       {current && (
-          <LocationForm
-            current={current}
-            createNew={createNew}
-            setLocation={setLocation}
-            setForceNext={setForceNext}
-            PickupDeliveryObject={PickupDeliveryObject}
-            setPickupDeliveryObject={setPickupDeliveryObject}
-          />
-      )}
+        <LocationForm
+          current={current}
+          createNew={createNew}
+          setLocation={setLocation}
+          setForceNext={setForceNext}
+          PickupDeliveryObject={PickupDeliveryObject}
+          setPickupDeliveryObject={setPickupDeliveryObject}
+        />
+      )} */}
     </div>
   );
 }
--- frontend/src/components/Locations/LocationForm.tsx
+++ frontend/src/components/Locations/LocationForm.tsx
@@ -1,95 +1,103 @@
-import React, { useState } from "react";
-import { toast } from "react-toastify";
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
 
 interface locale {
-	name: string,
-	LocationType: string,
-	EntityType: string,
-	FoodType: string,
-  DemographicName: string,
-	CombinedAreaName: string,
-	_id: string,
-  }
+  name: string;
+  LocationType: string;
+  EntityType: string;
+  FoodType: string;
+  DemographicName: string;
+  CombinedAreaName: string;
+  _id: string;
+}
 
 interface pickupDeliveryObjectSchema {
-    pickupOrDelivery: number,
-    id: String,
-    date: String,
-    driver: String,
-    vehicle: String,
-    name: String,
-    EntityType: String,
-    LocationType: String,
-    Demographic: String,
-    FoodType: String,
-    Area: String,
-    lbsDroppedOff: number
+  pickupOrDelivery: number;
+  id: String;
+  date: String;
+  driver: String;
+  vehicle: String;
+  name: String;
+  EntityType: String;
+  LocationType: String;
+  Demographic: String;
+  FoodType: String;
+  Area: String;
+  lbsDroppedOff: number;
 }
 
-
-interface Props{
-  current                 : locale,
-  createNew               : boolean,
-  setLocation             : Function,
-  PickupDeliveryObject    : pickupDeliveryObjectSchema,
-  setPickupDeliveryObject : Function,
-  setForceNext            : Function
+interface Props {
+  current: locale;
+  createNew: boolean;
+  setLocation: Function;
+  PickupDeliveryObject: pickupDeliveryObjectSchema;
+  setPickupDeliveryObject: Function;
+  setForceNext: Function;
 }
 
-const LocationForm = ({current, createNew, setLocation, PickupDeliveryObject, setPickupDeliveryObject, setForceNext} : Props) =>{
-  const [active, setActive] = useState(""); // State for radio buttons
+const LocationForm = ({
+  current,
+  createNew,
+  setLocation,
+  PickupDeliveryObject,
+  setPickupDeliveryObject,
+  setForceNext
+}: Props) => {
+  const [active, setActive] = useState(''); // State for radio buttons
   const [isClicked, setIsClicked] = useState(true); // State for radio buttons
 
   // State for if user is adding a new location
-  const [donorName, setName] = useState("");
-  const [donorLocationType, setDonorLocationType] = useState("");
-  const [donorEntityType, setDonorEntityType] = useState("");
-  const [food, setFoodType] = useState("");
-  const [demographic, setDemographic] = useState("");
-  const [area, setArea] = useState("");
+  const [donorName, setName] = useState('');
+  const [donorLocationType, setDonorLocationType] = useState('');
+  const [donorEntityType, setDonorEntityType] = useState('');
+  const [food, setFoodType] = useState('');
+  const [demographic, setDemographic] = useState('');
+  const [area, setArea] = useState('');
 
   const { name } = current;
 
   const submitPressed = () => {
-    if (createNew === false && active !== ""){
+    if (createNew === false && active !== '') {
       setPickupDeliveryObject({
         ...PickupDeliveryObject,
-        id : current._id,
-        name : current.name,
-        EntityType : current.EntityType,
-        LocationType : current.LocationType,
-        Demographic : current.DemographicName,
-        FoodType : active.toLowerCase(),
-        Area : current.CombinedAreaName
-      })
+        id: current._id,
+        name: current.name,
+        EntityType: current.EntityType,
+        LocationType: current.LocationType,
+        Demographic: current.DemographicName,
+        FoodType: active.toLowerCase(),
+        Area: current.CombinedAreaName
+      });
       setForceNext(true);
-    }
-    else if (createNew === true){
+    } else if (createNew === true) {
       setPickupDeliveryObject({
         ...PickupDeliveryObject,
         // id : ID?,
-        name : donorName,
-        EntityType : donorEntityType,
-        LocationType : donorLocationType,
-        Demographic : demographic,
-        FoodType : food,
-        Area : area
-      })
-      setForceNext(true); 
+        name: donorName,
+        EntityType: donorEntityType,
+        LocationType: donorLocationType,
+        Demographic: demographic,
+        FoodType: food,
+        Area: area
+      });
+      setForceNext(true);
+    } else {
+      toast.error('Please enter a food type.');
     }
-    else{
-      toast.error("Please enter a food type.")
-    }
-  }
+  };
 
   return (
     <div className="Form-main m-5 mb-10 flex justify-center flex-col">
-        <h2 className="text-4xl font-semibold mt-10 flex flex-start">
-          {createNew ? "Enter New Location:" : PickupDeliveryObject.pickupOrDelivery === 1 ? "Pickup Location:" : "Dropoff Location:"}
-        </h2>
-        {createNew ? (
-         PickupDeliveryObject.pickupOrDelivery === 1 ? (
-            <div className="text-4xl flex justify-center items-center grid grid-col gap-5">
+      <h2 className="text-4xl font-semibold mt-10 flex flex-start">
+        {createNew
+          ? 'Enter New Location:'
+          : PickupDeliveryObject.pickupOrDelivery === 1
+          ? 'Pickup Location:'
+          : 'Dropoff Location:'}
+      </h2>
+      {createNew ? (
+        PickupDeliveryObject.pickupOrDelivery === 1 ? (
+          <div className="text-4xl flex justify-center items-center grid grid-col gap-5">
             <input
               className="italic py-3 px-4 mt-3 rounded-lg shadow w-full text-left"
               type="text"
@@ -127,9 +135,10 @@ const LocationForm = ({current, createNew, setLocation, PickupDeliveryObject, se
               onChange={(e) => setArea(e.target.value)}
             />
           </div>
-         ): ( <div className="newLocation">
+        ) : (
+          <div className="newLocation">
             <input
-            className="italic py-3 px-4 mt-3 rounded-lg shadow w-full text-left"
+              className="italic py-3 px-4 mt-3 rounded-lg shadow w-full text-left"
               type="text"
               placeholder="Recipient name"
               name="name"
@@ -164,59 +173,85 @@ const LocationForm = ({current, createNew, setLocation, PickupDeliveryObject, se
               name="type"
               onChange={(e) => setArea(e.target.value)}
             />
-          </div>)
-        ) : (
-          <div className="existingLocation">
-            <input className="bg-white text-4xl w-full italic py-4 px-4 mt-3 rounded-lg shadow w-full text-left" type="text" placeholder={name} name="name" disabled={true} />
-            <div className="text-4xl font-semibold text-left pt-10">Food type:</div>
-            <div className="text-3xl text-left ml-20 m-4 py-4">
-              {current &&
-                 <div className="flex items-center mr-4 mb-4">
-                      <input className='mx-3 my-5 hidden'
-                            id="radio1"
-                            type="radio"
-                            name="foodType"
-                            value={current.FoodType}
-                            onClick={() => {
-                              setActive(current.FoodType);
-                              setIsClicked(true);}}
-                            checked/>
-                        <label htmlFor="radio1" className="flex items-center cursor-pointer text-3xl">
-                        <span className="w-8 h-8 inline-block mr-2 bg-white rounded-full border border-grey flex-no-shrink"></span>
-                        {current.FoodType}</label>
-                  </div>
-              }
-              {current && (
-                <div>
-                  <input
-                    className='mx-3 my-5 hidden'
-                    id="radio2"
-                    type="radio"
-                    name="foodType"
-                    onClick={() => setIsClicked(false)}
-                    checked
-                  />
-                  <label htmlFor="radio2" className="flex items-center cursor-pointer text-3xl">
-                        <span className="w-8 h-8 inline-block mr-2 bg-white rounded-full border border-grey flex-no-shrink"></span>
-                        {"Other"}</label>
-                  <input className="bg-white ml-2 text-4xl w-full italic py-4 px-4 mt-2 rounded-lg shadow w-full text-left"
-                    type="text"
-                    disabled={isClicked}
-                    onChange={(e) => setActive(e.target.value)}
-                    placeholder='Please Specify'
-                  />{" "}
-                </div>
-              )}
-            </div>
           </div>
-        )}
-
-        <div>
-          <button className="bg-amber-500 rounded-full w-full mt-5 p-3 text-3xl text-white font-semibold shadow" onClick={() => submitPressed()}>Continue</button>
+        )
+      ) : (
+        <div className="existingLocation">
+          <input
+            className="bg-white text-4xl w-full italic py-4 px-4 mt-3 rounded-lg shadow w-full text-left"
+            type="text"
+            placeholder={name}
+            name="name"
+            disabled={true}
+          />
+          <div className="text-4xl font-semibold text-left pt-10">
+            Food type:
+          </div>
+          <div className="text-3xl text-left ml-20 m-4 py-4">
+            {current && (
+              <div className="flex items-center mr-4 mb-4">
+                <input
+                  className="mx-3 my-5 hidden"
+                  id="radio1"
+                  type="radio"
+                  name="foodType"
+                  value={current.FoodType}
+                  onClick={() => {
+                    setActive(current.FoodType);
+                    setIsClicked(true);
+                  }}
+                  checked
+                />
+                <label
+                  htmlFor="radio1"
+                  className="flex items-center cursor-pointer text-3xl"
+                >
+                  <span className="w-8 h-8 inline-block mr-2 bg-white rounded-full border border-grey flex-no-shrink"></span>
+                  {current.FoodType}
+                </label>
+              </div>
+            )}
+            {current && (
+              <div>
+                <input
+                  className="mx-3 my-5 hidden"
+                  id="radio2"
+                  type="radio"
+                  name="foodType"
+                  onClick={() => setIsClicked(false)}
+                  checked
+                />
+                <label
+                  htmlFor="radio2"
+                  className="flex items-center cursor-pointer text-3xl"
+                >
+                  <span className="w-8 h-8 inline-block mr-2 bg-white rounded-full border border-grey flex-no-shrink"></span>
+                  {'Other'}
+                </label>
+                <input
+                  className="bg-white ml-2 text-4xl w-full italic py-4 px-4 mt-2 rounded-lg shadow w-full text-left"
+                  type="text"
+                  disabled={isClicked}
+                  onChange={(e) => setActive(e.target.value)}
+                  placeholder="Please Specify"
+                />{' '}
+              </div>
+            )}
+          </div>
         </div>
+      )}
+
+      <div>
+        <button
+          className="bg-amber-500 rounded-full w-full mt-5 p-3 text-3xl text-white font-semibold shadow"
+          onClick={() => submitPressed()}
+        >
+          Continue
+        </button>
+      </div>
       {/* </form> */}
     </div>
   );
-}
+};
 
 export default LocationForm;
