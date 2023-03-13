4509dd521b35f6c739bde7db1e12c21bb1e18036
Tarini Srikanth <tareene@gmail.com>
2023-03-13 16:06:48 -0700

    [133] feat: added checkboxes for food type (#146)
    * 133-added checkboxes for food type
    
    * 133-fixed reviews from pull request
    
    * fixed reviews from pull request
    
    * 133-fixed produce adding multipl times issue
    
    * 133-fixed other empty value issue
    
    * chore: linting
    
    * 133-updated for more generalized checking logic
    
    * 133-updated for more flexuble isChecked logic
    
    * fix: linting and fixing logic
    
    ---------
    
    Co-authored-by: esauerbo <esauerbo@calpoly.edu>

--- frontend/src/components/Locations/LocationForm.tsx
+++ frontend/src/components/Locations/LocationForm.tsx
@@ -1,108 +1,153 @@
-import React, { useState } from "react";
-import { toast } from "react-toastify";
+/* eslint-disable no-nested-ternary */
+import { useState } from 'react';
+import { toast } from 'react-toastify';
 
-interface locale {
-	name: string,
-	LocationType: string,
-	EntityType: string,
-	FoodType: string,
-  DemographicName: string,
-	CombinedAreaName: string,
-	_id: string,
-  }
-
-interface pickupDeliveryObjectSchema {
-    pickupOrDelivery: number,
-    id: String,
-    date: String,
-    driver: String,
-    vehicle: String,
-    name: String,
-    EntityType: String,
-    LocationType: String,
-    Demographic: String,
-    FoodType: String,
-    Area: String,
-    lbsDroppedOff: number
+interface Locale {
+  name: string;
+  LocationType: string;
+  EntityType: string;
+  FoodType: string;
+  DemographicName: string;
+  CombinedAreaName: string;
+  _id: string;
 }
 
-
-interface Props{
-  current                 : locale,
-  createNew               : boolean,
-  setLocation             : Function,
-  PickupDeliveryObject    : pickupDeliveryObjectSchema,
-  setPickupDeliveryObject : Function,
-  setForceNext            : Function
+interface PickupDeliveryObjectSchema {
+  pickupOrDelivery: number;
+  id: string;
+  date: string;
+  driver: string;
+  vehicle: string;
+  name: string;
+  EntityType: string;
+  LocationType: string;
+  Demographic: string;
+  FoodType: string;
+  Area: string;
+  lbsDroppedOff: number;
 }
 
-const LocationForm = ({current, createNew, setLocation, PickupDeliveryObject, setPickupDeliveryObject, setForceNext} : Props) =>{
-  const [active, setActive] = useState(""); // State for radio buttons
-  const [isClicked, setIsClicked] = useState(true); // State for radio buttons
+interface Props {
+  current: Locale;
+  createNew: boolean;
+  setLocation: Function;
+  PickupDeliveryObject: PickupDeliveryObjectSchema;
+  setPickupDeliveryObject: Function;
+  setForceNext: Function;
+}
+
+function LocationForm({
+  current,
+  createNew,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  setLocation,
+  PickupDeliveryObject,
+  setPickupDeliveryObject,
+  setForceNext
+}: Props) {
+  const [isOtherClicked, setOtherClicked] = useState(false);
+  const [checked, setChecked] = useState<string[]>([]);
+  // State for checkbox buttons
 
   // State for if user is adding a new location
-  const [donorName, setName] = useState("");
-  const [donorLocationType, setDonorLocationType] = useState("");
-  const [donorEntityType, setDonorEntityType] = useState("");
-  const [food, setFoodType] = useState("");
-  const [demographic, setDemographic] = useState("");
-  const [area, setArea] = useState("");
+  const [donorName, setName] = useState('');
+  const [donorLocationType, setDonorLocationType] = useState('');
+  const [donorEntityType, setDonorEntityType] = useState('');
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [food, setFoodType] = useState('');
+  const [demographic, setDemographic] = useState('');
+  const [area, setArea] = useState('');
+  const [input, setInput] = useState('');
 
   const { name } = current;
 
   const submitPressed = () => {
-    if (createNew === false && active !== ""){
-      setPickupDeliveryObject({
-        ...PickupDeliveryObject,
-        id : current._id,
-        name : current.name,
-        EntityType : current.EntityType,
-        LocationType : current.LocationType,
-        Demographic : current.DemographicName,
-        FoodType : active.toLowerCase(),
-        Area : current.CombinedAreaName
-      })
-      setForceNext(true);
+    let foodTypes = checked;
+    let isValid = checked.length > 0;
+
+    if (isOtherClicked) {
+      if (input.trim().length === 0) {
+        isValid = false;
+      } else {
+        setChecked([...checked, input]);
+        // react updates state variables asynchronously so this is to get most recent food list
+        foodTypes = [...checked, input];
+      }
     }
-    else if (createNew === true){
-      setPickupDeliveryObject({
-        ...PickupDeliveryObject,
-        // id : ID?,
-        name : donorName,
-        EntityType : donorEntityType,
-        LocationType : donorLocationType,
-        Demographic : demographic,
-        FoodType : food,
-        Area : area
-      })
-      setForceNext(true); 
+
+    if (isValid) {
+      console.log(`submitting form with data: ${foodTypes}`);
+      if (createNew === false) {
+        setPickupDeliveryObject({
+          ...PickupDeliveryObject,
+          id: current._id,
+          name: current.name,
+          EntityType: current.EntityType,
+          LocationType: current.LocationType,
+          Demographic: current.DemographicName,
+          FoodType: foodTypes,
+          Area: current.CombinedAreaName
+        });
+        setForceNext(true);
+      } else if (createNew === true) {
+        setPickupDeliveryObject({
+          ...PickupDeliveryObject,
+          // id : ID?,
+          name: donorName,
+          EntityType: donorEntityType,
+          LocationType: donorLocationType,
+          Demographic: demographic,
+          FoodType: foodTypes,
+          Area: area
+        });
+        setForceNext(true);
+      }
+    } else {
+      toast.error('Please enter value for Other.');
     }
-    else{
-      toast.error("Please enter a food type.")
+  };
+
+  const handleClick = () => {
+    let updatedList = [...checked];
+
+    if (updatedList.indexOf(current.FoodType) !== -1) {
+      // if currentFoodtype is in the checked array, remove it
+      updatedList.splice(updatedList.indexOf(current.FoodType), 1);
+    } else {
+      updatedList = [...checked, current.FoodType];
     }
-  }
+    setChecked(updatedList);
+  };
 
   return (
     <div className="Form-main m-5 mb-10 flex justify-center flex-col">
-        <h2 className="text-4xl font-semibold mt-10 flex flex-start">
-          {createNew ? "Enter New Location:" : PickupDeliveryObject.pickupOrDelivery === 1 ? "Pickup Location:" : "Dropoff Location:"}
-        </h2>
-        {createNew ? (
-         PickupDeliveryObject.pickupOrDelivery === 1 ? (
-            <div className="text-4xl flex justify-center items-center grid grid-col gap-5">
+      <h2 className="text-4xl font-semibold mt-10 flex flex-start">
+        {createNew
+          ? 'Enter New Location:'
+          : PickupDeliveryObject.pickupOrDelivery === 1
+          ? 'Pickup Location:'
+          : 'Dropoff Location:'}
+      </h2>
+      {createNew ? (
+        PickupDeliveryObject.pickupOrDelivery === 1 ? (
+          <div className="text-4xl flex justify-center items-center grid grid-col gap-5">
             <input
               className="italic py-3 px-4 mt-3 rounded-lg shadow w-full text-left"
               type="text"
               placeholder="Donor name"
               name="name"
-              onChange={(e) => setName(e.target.value)}
+              onChange={(e: { target: { value: any } }) =>
+                setName(e.target.value)
+              }
             />
             <input
               className="italic py-3 px-4 mt-3 rounded-lg shadow w-full text-left"
               type="text"
               placeholder="Donor Location Type"
               name="location"
-              onChange={(e) => setDonorLocationType(e.target.value)}
+              onChange={(e: { target: { value: any } }) =>
+                setDonorLocationType(e.target.value)
+              }
             />
 
             <input
@@ -110,30 +155,39 @@ const LocationForm = ({current, createNew, setLocation, PickupDeliveryObject, se
               type="text"
               placeholder="Entity Type"
               name="location"
-              onChange={(e) => setDonorEntityType(e.target.value)}
+              onChange={(e: { target: { value: any } }) =>
+                setDonorEntityType(e.target.value)
+              }
             />
             <input
               className="italic py-3 px-4 mt-3 rounded-lg shadow w-full text-left"
               type="text"
               placeholder="Food Type"
               name="type"
-              onChange={(e) => setFoodType(e.target.value)}
+              onChange={(e: { target: { value: any } }) =>
+                setFoodType(e.target.value)
+              }
             />
             <input
               className="italic py-3 px-4 mt-3 rounded-lg shadow w-full text-left"
               type="text"
               placeholder="Area"
               name="type"
-              onChange={(e) => setArea(e.target.value)}
+              onChange={(e: { target: { value: any } }) =>
+                setArea(e.target.value)
+              }
             />
           </div>
-         ): ( <div className="newLocation">
+        ) : (
+          <div className="newLocation">
             <input
-            className="italic py-3 px-4 mt-3 rounded-lg shadow w-full text-left"
+              className="italic py-3 px-4 mt-3 rounded-lg shadow w-full text-left"
               type="text"
               placeholder="Recipient name"
               name="name"
-              onChange={(e) => setName(e.target.value)}
+              onChange={(e: { target: { value: any } }) =>
+                setName(e.target.value)
+              }
             />
 
             <input
@@ -141,79 +195,114 @@ const LocationForm = ({current, createNew, setLocation, PickupDeliveryObject, se
               type="text"
               placeholder="Entity Type"
               name="location"
-              onChange={(e) => setDonorEntityType(e.target.value)}
+              onChange={(e: { target: { value: any } }) =>
+                setDonorEntityType(e.target.value)
+              }
             />
             <input
               className="italic py-3 px-4 mt-3 rounded-lg shadow w-full text-left"
               type="text"
               placeholder="Food Type"
               name="type"
-              onChange={(e) => setFoodType(e.target.value)}
+              onChange={(e: { target: { value: any } }) =>
+                setFoodType(e.target.value)
+              }
             />
             <input
               className="italic py-3 px-4 mt-3 rounded-lg shadow w-full text-left"
               type="text"
               placeholder="Demographic"
               name="location"
-              onChange={(e) => setDemographic(e.target.value)}
+              onChange={(e: { target: { value: any } }) =>
+                setDemographic(e.target.value)
+              }
             />
             <input
               className="italic py-3 px-4 mt-3 rounded-lg shadow w-full text-left"
               type="text"
               placeholder="Area"
               name="type"
-              onChange={(e) => setArea(e.target.value)}
-            />
-          </div>)
-        ) : (
-          <div className="existingLocation">
-            <input className="bg-white text-4xl w-full italic py-4 px-4 mt-3 rounded-lg shadow w-full text-left" type="text" placeholder={name} name="name" disabled={true} />
-            <div className="text-4xl font-semibold text-left pt-10">Food type:</div>
-            <div className="text-3xl text-left ml-20 m-4 py-4">
-              {current &&
-                 <div className="flex items-center mr-4 mb-4">
-                      <input className='mx-3 my-5 hidden'
-                            id="radio1"
-                            type="radio"
-                            name="foodType"
-                            value={current.FoodType}
-                            onClick={() => {
-                              setActive(current.FoodType);
-                              setIsClicked(true);}}
-                            checked/>
-                        <label htmlFor="radio1" className="flex items-center cursor-pointer text-3xl">
-                        <span className="w-8 h-8 inline-block mr-2 bg-white rounded-full border border-grey flex-no-shrink"></span>
-                        {current.FoodType}</label>
-                  </div>
+              onChange={(e: { target: { value: any } }) =>
+                setArea(e.target.value)
               }
-              {current && (
-                <div>
-                  <input
-                    className='mx-3 my-5 hidden'
-                    id="radio2"
-                    type="radio"
-                    name="foodType"
-                    onClick={() => setIsClicked(false)}
-                    checked
-                  />
-                  <label htmlFor="radio2" className="flex items-center cursor-pointer text-3xl">
-                        <span className="w-8 h-8 inline-block mr-2 bg-white rounded-full border border-grey flex-no-shrink"></span>
-                        {"Other"}</label>
-                  <input className="bg-white ml-2 text-4xl w-full italic py-4 px-4 mt-2 rounded-lg shadow w-full text-left"
-                    type="text"
-                    disabled={isClicked}
-                    onChange={(e) => setActive(e.target.value)}
-                    placeholder='Please Specify'
-                  />{" "}
-                </div>
-              )}
-            </div>
+            />
           </div>
-        )}
-
-        <div>
-          <button className="bg-amber-500 rounded-full w-full mt-5 p-3 text-3xl text-white font-semibold shadow" onClick={() => submitPressed()}>Continue</button>
+        )
+      ) : (
+        <div className="existingLocation">
+          <input
+            className="bg-white text-4xl w-full italic py-4 px-4 mt-3 rounded-lg shadow w-full text-left"
+            type="text"
+            placeholder={name}
+            name="name"
+            disabled
+          />
+          <div className="text-4xl font-semibold text-left pt-10">
+            Food type:
+          </div>
+          <div className="text-3xl text-left ml-20 m-4 py-4">
+            {current && (
+              <div className="flex items-center mb-4">
+                <input
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 w-8 h-8 inline-block mr-2 bg-white border border-grey flex-no-shrink"
+                  id="checkbox"
+                  type="checkbox"
+                  name="foodType"
+                  value={current.FoodType}
+                  onClick={handleClick}
+                />
+                <label
+                  htmlFor="checkbox"
+                  className="flex items-center cursor-pointer text-3xl"
+                >
+                  {current.FoodType}
+                </label>
+              </div>
+            )}
+            {current && (
+              <div>
+                <input
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600  w-8 h-8 inline-block mr-2 bg-white border border-grey flex-no-shrink"
+                  id="checkbox2"
+                  type="checkbox"
+                  name="foodType"
+                  value="Other"
+                  onClick={() => {
+                    setOtherClicked(!isOtherClicked);
+                  }}
+                />
+                <label htmlFor="checkbox2" className="cursor-pointer text-3xl">
+                  {'Other'}{' '}
+                </label>
+                <input
+                  className="bg-white ml-2 text-4xl w-full italic py-4 px-4 mt-2 rounded-lg shadow w-full text-left"
+                  type="text"
+                  disabled={!isOtherClicked}
+                  onChange={(e) => {
+                    setInput(e.target.value);
+                  }}
+                  placeholder="Please Specify"
+                />{' '}
+              </div>
+            )}
+          </div>
         </div>
+      )}
+
+      <div>
+        <button
+          type="submit"
+          disabled={
+            checked.length === 0 && !isOtherClicked
+          } /* disable if nothing is clicked */
+          className="bg-amber-500 rounded-full w-full mt-5 p-3 text-3xl text-white font-semibold shadow"
+          onClick={() => {
+            submitPressed();
+          }}
+        >
+          Continue
+        </button>
+      </div>
       {/* </form> */}
     </div>
   );
