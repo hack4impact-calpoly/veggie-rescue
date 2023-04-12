c37c952e45bebf2f0744728d3eeb94678097acad
Connor OBrien <connor.p.obrien@gmail.com>
2023-04-11 18:58:08 -0700

    refactor - editField

--- backend/controllers/fieldController.js
+++ backend/controllers/fieldController.js
@@ -64,52 +64,41 @@ const createField = asyncHandler(async (req, res) => {
 // @route PUT /api/fields/:name
 // @access Private -> Admin only
 const editField = asyncHandler(async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+  const admin = await Admin.findById(req.admin.id);
+  if (!admin) {
+    res.status(401);
+    throw new Error("Admin not found");
   }
+  const body = req.body;
 
-  const { name, oldItem, newItem } = req.body;
-  try {
-    const field = await Field.findOneAndUpdate(
-      { [name]: oldItem },
-      { $set: { [`${name}.$`]: newItem } },
-      { new: true }
-    );
-    if (!field) {
-      return res.status(404).json({ msg: 'Field item not found' });
-    }
-    res.json(field);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
+  if (body.name) Field.name = body.name;
+  if (body.item) Field.item = body.item;
+
+  await Field.findByIdAndUpdate(req.params.id, Field);
+  return res.status(201).json(Field);
 });
 
 // @desc Delete field item
 // @route DELETE /api/fields/:name
 // @access Private -> Admin only
 const deleteField = asyncHandler(async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+    const admin = await Admin.findById(req.admin.id);
+    if (!admin) {
+      res.status(401);
+      throw new Error("Admin not found");
     }
-  
-    const { name } = req.params;
-    const { items } = req.body;
-    try {
-      const field = await Field.findOneAndUpdate(
-        { [name]: { $in: items } },
-        { $pull: { [name]: { $in: items } } },
-        { new: true }
-      );
-      if (!field) {
-        return res.status(404).json({ msg: `No such field with name ${name} and items ${items.join(', ')}` });
-      }
-      res.json(field);
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server Error');
+
+    const field = await Donor.findById(req.params.id);
+
+    if (!field) {
+      res.status(404);
+      throw new Error("Field not found");
     }
-  });
+    
+    await field.remove();
+
+    res.status(200).json({success: true})
+});
+
+
   
