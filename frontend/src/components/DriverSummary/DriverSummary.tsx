dbab6dc6e3593793a3c0c5a24b1e6d4b07247eb7
lemezze <letimezzetti@gmail.com>
2023-04-13 17:02:24 -0700

    feat: changed text area

--- frontend/src/components/DriverSummary/DriverSummary.tsx
+++ frontend/src/components/DriverSummary/DriverSummary.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import "./DriverSummary.css"
 import NavBarButton_DARK from '../../imgs/button_dark_left.svg';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
 
 function DriverSummary() {
   return <div className = "all-page">
@@ -8,28 +10,28 @@ function DriverSummary() {
       <img className='backButton' src={NavBarButton_DARK} alt="Back Button"/>
     </button>
 
-    <p className = "punchOutHeader">Punch Out</p>
+    <p className = "punchOutHeader"> Punch Out </p>
 
-    <h1 className = "goodbyeMessage">Bye Diana!</h1>
-    <h2 className = "summary">Today's Summary</h2>
+    <h1 className = "goodbyeMessage"> Bye Diana! </h1>
+    <h2 className = "summary"> Today's Summary </h2>
+
+  {/* Get Clock in/Clock Out */}
 
     <div className = "weight-container"> 
-      <p className = "weight">100</p>
+      <p className = "weight"> 100</p>
       <p className = "units">lbs</p>
     </div>
 
     <h3 className = "droppedOff">Dropped off</h3>
 
-    <div className = "discrepancy">
-      Discrepancies
-      <label>
-          <textarea className = "discrepancy-container">
-            The Los Alamos Foundation
-          </textarea>
-      </label>
-    </div>
+    <label className = "discrepancy"> Discrepancies </label>
 
-    <button className = "signOutButton">Sign Out</button>
+    <form>
+      <textarea className = "discrepancy-container" id="discrepancies" placeholder="Los Alamos Foundation">
+      </textarea>
+    </form>
+
+    <button className = "signOutButton"> Sign Out </button>
 
   </div>
 }
