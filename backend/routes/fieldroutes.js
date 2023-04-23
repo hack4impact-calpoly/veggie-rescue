b356a026124f53cb57fc335a98fb8e5cf4510d7a
Connor OBrien <connor.p.obrien@gmail.com>
2023-04-22 19:33:57 -0700

    feat - field routes

--- backend/routes/fieldroutes.js
+++ backend/routes/fieldroutes.js
@@ -9,4 +9,19 @@ const {
 } = require("../controllers/fieldController");
 const { protectAdmin } = require("../middleware/authMiddleware");
 
+// get all fields
+router.get("/", protectAdmin, getFields); 
 
+// get field by name
+router.get("/:name", protectAdmin, getFieldByName); 
+
+// create field
+router.post("/", protectAdmin, createField);
+
+// edit field item
+router.put("/:name", protectAdmin, editField);
+
+// delete field
+router.delete("/:name", protectAdmin, deleteField);
+
+module.exports = router;
