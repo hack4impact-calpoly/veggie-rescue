8ee3b23a7695d636d50c5d31a9a31ef6a31ed4c9
esauerbo <esauerbo@calpoly.edu>
2023-04-26 23:15:37 -0700

    fix: updating paths in logs controller

--- backend/controllers/logsController.js
+++ backend/controllers/logsController.js
@@ -1,7 +1,7 @@
 const PickupLog = require("../models/pickupLogSchema");
 const DropoffLog = require("../models/dropoffLogSchema");
-const Admin = require("../models/adminModel.js");
-const Driver = require('../models/driverModel.js')
+const Admin = require("../models/adminSchema");
+const Driver = require("../models/driverSchema");
 
 // @desc Get all pickup logs
 // @route GET : /api/pickup
