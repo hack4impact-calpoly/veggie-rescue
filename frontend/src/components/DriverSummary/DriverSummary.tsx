12bc5571c58ae59ad401b41ae24b3f6a1d76482c
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2023-04-26 23:01:33 -0700

    linting errors for tsx and stashing conflict on CSS file

--- frontend/src/components/DriverSummary/DriverSummary.tsx
+++ frontend/src/components/DriverSummary/DriverSummary.tsx
@@ -1,39 +1,43 @@
-import React from 'react';
-import "./DriverSummary.css"
+import './DriverSummary.css';
 import NavBarButton_DARK from '../../imgs/button_dark_left.svg';
-import { useState } from 'react';
-import { toast } from 'react-toastify';
 
 function DriverSummary() {
-  return <div className = "all-page">
-    <button id="icon">
-      <img className='backButton' src={NavBarButton_DARK} alt="Back Button"/>
-    </button>
+  return (
+    <div className="all-page">
+      <button type="button" id="icon">
+        <img className="backButton" src={NavBarButton_DARK} alt="Back Button" />
+      </button>
 
-    <p className = "punchOutHeader"> Punch Out </p>
+      <p className="punchOutHeader"> Punch Out </p>
 
-    <h1 className = "goodbyeMessage"> Bye Diana! </h1>
-    <h2 className = "summary"> Today's Summary </h2>
+      <h1 className="goodbyeMessage"> Bye Diana! </h1>
+      <h2 className="summary"> Today&apos;s Summary </h2>
 
-  {/* Get Clock in/Clock Out */}
+      {/* Get Clock in/Clock Out */}
 
-    <div className = "weight-container"> 
-      <p className = "weight"> 100</p>
-      <p className = "units">lbs</p>
+      <div className="weight-container">
+        <p className="weight"> 100</p>
+        <p className="units">lbs</p>
+      </div>
+
+      <h3 className="droppedOff">Dropped off</h3>
+
+      <h4 className="discrepancy">Discrepancies </h4>
+
+      <form>
+        <textarea
+          className="discrepancy-container"
+          id="discrepancies"
+          placeholder="Los Alamos Foundation"
+        />
+      </form>
+
+      <button type="button" className="signOutButton">
+        {' '}
+        Sign Out{' '}
+      </button>
     </div>
-
-    <h3 className = "droppedOff">Dropped off</h3>
-
-    <label className = "discrepancy"> Discrepancies </label>
-
-    <form>
-      <textarea className = "discrepancy-container" id="discrepancies" placeholder="Los Alamos Foundation">
-      </textarea>
-    </form>
-
-    <button className = "signOutButton"> Sign Out </button>
-
-  </div>
+  );
 }
 
 export default DriverSummary;
