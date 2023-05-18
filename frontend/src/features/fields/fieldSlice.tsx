2747c77bbb2c40138ff3f1375dbb264a128f5733
Connor OBrien <connor.p.obrien@gmail.com>
2023-05-17 17:41:08 -0700

    feat - getFields, pending, fulfilled, rejected

--- frontend/src/features/fields/fieldSlice.tsx
+++ frontend/src/features/fields/fieldSlice.tsx
@@ -46,10 +46,70 @@ const initialState: FieldState = {
     message: ''
 };
 
+export const getFields = createAsyncThunk(
+    'api/location/getFields',
+    async (_, thunkAPI) => {
+        try {
+            const state = thunkAPI.getState() as RootState;
+            let { token } = state.driverAuth.driver;
+            if (!token) {
+                token = state.adminAuth.admin.token;
+            }
 
+            return await fieldService.getFields(token);
+        } catch (error: any) {
+            const message =
+                (error.response &&
+                    error.response.data &&
+                    error.response.data.message) ||
+                error.message ||
+                error.toString();
+
+            return thunkAPI.rejectWithValue(message);
+        }
+    }
+);
+
+export const fieldSlice = createSlice({
+    name: 'fields',
+    initialState,
+    reducers: {
+        reset: (state) => ({
+            ...state,
+            isLoading: false,
+            isError: false,
+            isSuccess: false,
+            message: ''
+        }),
+        clear: () => initialState
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(getFields.pending, (state) => ({
+                ...state,
+                isLoading: true
+            }))
+            .addCase(getFields.fulfilled, (state, action) => ({
+                ...state,
+                isLoading: false,
+                isSuccess: true,
+                fields: action.payload
+            }))
+            .addCase(getFields.rejected, (state, action) => ({
+                ...state,
+                isLoading: false,
+                isError: true,
+                message: action.payload
+            }))
+    }
+});
+
+
+  
 
 
 
 
 
 export const { reset, clear } = fieldSlice.actions;
+export default fieldSlice.reducer;
