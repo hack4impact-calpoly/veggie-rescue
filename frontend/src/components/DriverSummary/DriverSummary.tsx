ddb124e581f95e401aedab89f71756566c2c9e07
canbarci <canbarci@calpoly.edu>
2023-05-24 01:45:55 -0700

    refactor: updated driver summary page ui

--- frontend/src/components/DriverSummary/DriverSummary.tsx
+++ frontend/src/components/DriverSummary/DriverSummary.tsx
@@ -3,47 +3,56 @@ import NavBarButton_DARK from '../../imgs/button_dark_left.svg';
 
 function DriverSummary() {
   return (
-    <div className="container p-2">
-      <div className="flex flex-row mx-3 mt-16 mb-7 md:text-5xl  text-3xl font-semibold text-lime-900 md:w-3/5 w-5/6">
+    <div className="full-page">
+      <div className="header">
         <button
-          className="items-center justify-center "
+          className="back-button"
           id="icon"
           type="button"
           // onClick={backFunc}
         >
           <img className="h-16" src={NavBarButton_DARK} alt="leftButton" />
         </button>
-        <div className="flex">
-          {/* <span className="capitalize">{name}</span> */}
-          <span>Bye Diana!</span>
+        <h1> Bye Diana! </h1>
+      </div>
+
+      <h2 className="summary-title"> Today&apos;s Summary </h2>
+
+      {/* Add clock in/clock Out */}
+
+      <div className="summaries">
+        <div className="summary">
+          <div className="weight-container">
+            <p className="weight"> 100</p>
+            <p className="units">lbs</p>
+          </div>
+
+          <h3>Dropped off</h3>
+        </div>
+
+        <div className="summary">
+          <div className="weight-container">
+            <p className="weight"> 50</p>
+            <p className="units">lbs</p>
+          </div>
+
+          <h3>Picked up</h3>
+        </div>
+
+        <div className="summary">
+          <div className="weight-container">
+            <p className="weight"> 50</p>
+            <p className="units">lbs</p>
+          </div>
+
+          <h3>Left in Vehicle</h3>
         </div>
       </div>
 
-      {/* <h1 className="goodbyeMessage"> Bye Diana! </h1> */}
-      <h2 className="summary"> Today&apos;s Summary </h2>
+      {/* Add confirmation/error checking functionality */}
 
-      {/* Get Clock in/Clock Out */}
-
-      <div className="weight-container">
-        <p className="weight"> 100</p>
-        <p className="units">lbs</p>
-      </div>
-
-      <h3 className="droppedOff">Dropped off</h3>
-
-      <h4 className="discrepancy">Discrepancies </h4>
-
-      <form>
-        <textarea
-          className="discrepancy-container"
-          id="discrepancies"
-          placeholder="Los Alamos Foundation"
-        />
-      </form>
-
-      <button type="button" className="signOutButton">
-        {' '}
-        Sign Out{' '}
+      <button type="button" className="confirm-button">
+        Confirm Punch Out
       </button>
     </div>
   );
